6792a4b85f93452f6d876abc096fbb80a7e9cd22
Iván del Río <64600819+ivanDRG@users.noreply.github.com>
2020-05-11 13:43:51 +0200

    File upload

---
+++ index.js
@@ -0,0 +1,67 @@
+//Buttons
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i<numberOfButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonText = this.innerHTML;
+    makeSound(buttonText);
+    animationKey(buttonText);
+  });
+  //Keyboard
+   document.addEventListener("keydown", function(event){
+    var keyPressed = event.key;
+    makeSound(keyPressed);
+    animationKey(keyPressed);
+   })
+
+   //Function
+   function makeSound (key){
+   switch (key) {
+     case "w":
+     var sound = new Audio("sounds/kick-bass.mp3");
+     sound.play();
+     break;
+
+     case "a":
+     var sound = new Audio("sounds/snare.mp3");
+     sound.play();
+     break;
+
+     case "s":
+     var sound = new Audio("sounds/crash.mp3");
+     sound.play();
+     break;
+
+     case "d":
+     var sound = new Audio("sounds/tom-1.mp3");
+     sound.play();
+     break;
+
+     case "j":
+     var sound = new Audio("sounds/tom-2.mp3");
+     sound.play();
+     break;
+
+     case "k":
+     var sound = new Audio("sounds/tom-3.mp3");
+     sound.play();
+     break;
+
+     case "l":
+     var sound = new Audio("sounds/tom-4.mp3");
+     sound.play();
+     break;
+
+     default:
+     console.log(buttonTex);
+   }
+ }
+ function animationKey(currentKey){
+   var activeKey = document.querySelector('.' + currentKey);
+   activeKey.classList.add("pressed");
+   setTimeout(function(){
+    activeKey.classList.remove("pressed");
+  }, 100);
+
+ }
+
+}
